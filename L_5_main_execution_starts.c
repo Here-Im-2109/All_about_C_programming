0735ab2ff10908453bf41d47a908ec207931254b
Arpan Chandra <1.arpanchandra@gmail.com>
2024-11-13 11:47:07 +0530

    adding -> main execution starting file.

---
+++ L_5_main_execution_starts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() // Main execution starts
+{
+    printf(
+        "\nHello World !!\n\n"
+        )
+        ;
+    return 
+    0
+    ;
+
+    // identifier, constant, string literal & symbols
+}
