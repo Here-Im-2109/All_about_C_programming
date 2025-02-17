e6fb873d8f7ce8fa45864186374f93d13628dd1a
Arpan Chandra <1.arpanchandra@gmail.com>
2025-02-17 17:26:14 +0530

    Adding - 01_str.c - Basic of String.

---
+++ DSA_updates/01_str.c
@@ -0,0 +1,14 @@
+//      Purpose: Basic of String.
+
+#include<stdio.h>
+
+int main()
+{
+    char name[30];
+    printf("\nEnter name: ");
+    gets(name);
+    printf("\nYour Name is : ");
+    puts(name);
+    printf("\n");
+    return 0;
+}
