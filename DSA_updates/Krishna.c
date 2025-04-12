78d45861c8a5ff8d89e0de2efe62c059f18ecd90
Arpan Chandra <1.arpanchandra@gmail.com>
2025-04-12 23:19:34 +0530

    Adding - krishna.c - purpose: To create a pattern using of KRISHNA IS LOVE using C language.

---
+++ DSA_updates/Krishna.c
@@ -0,0 +1,23 @@
+// purpose: To create a pattern using of "KRISHNA IS LOVE" using C language.
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int counter = 0; // Added a counter variable
+
+    while (counter < 10) // Limit the loop to 10 iterations
+    {
+        printf("  KRISHNA IS LOVE\n"); // Print the message
+        counter++; // Increment the counter
+    }
+
+    printf("\nEnd of loop. KRISHNA IS FOREVER LOVE.\n"); // Final message after the loop ends
+
+    return 0;
+}
