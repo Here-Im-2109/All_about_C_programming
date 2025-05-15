afbda4f4ac7c7ab7719057e36047d3e106313898
Arpan Chandra <1.arpanchandra@gmail.com>
2025-05-15 22:10:43 +0530

    adding round up any number in C programming - Purpose is to round up the number to the nearest integer

---
+++ DSA_updates/round_up_the_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	float i = 9.2;
+	printf("\n%f",i);
+	printf("\n%2f",i);
+	printf("\n%02f",i);
+	printf("\n%02.1f",i);
+	printf("\n%02.10f",i);
+	printf("\n%09.2f",i);
+	printf("\n%2.4f",i);
+	printf("\n%19.4f",i);
+	printf("\n%1.6f",i);
+}
