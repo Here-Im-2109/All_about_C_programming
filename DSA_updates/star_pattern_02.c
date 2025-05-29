d4832813cdd5990deaecd91bbedb45f6f36e663d
Arpan Chandra <1.arpanchandra@gmail.com>
2025-05-30 00:01:40 +0530

    Adding pattern 02

---
+++ DSA_updates/star_pattern_02.c
@@ -0,0 +1,27 @@
+// purpose: This code prints a pyramid star pattern based on user input.
+
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    int line = 1;
+    while (line <= num)
+    {	
+        int space = 0;
+        while (space < (num - line))
+        {
+            printf(" ");
+            space = space + 1;
+        }
+        int star = 0;
+        while (star < (2 * line) - 1)
+        {
+            printf("*");
+            star = star + 1;
+        }
+        line++;
+        printf("\n");
+    }
+}
